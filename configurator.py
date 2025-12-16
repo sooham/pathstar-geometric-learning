132272b18f8a9f94da6f3564057003170d704af7
sooham <rafizsooham@gmail.com>
2025-12-16 11:42:23 -0500

    fix: configurator

--- configurator.py
+++ configurator.py
@@ -1,45 +1,29 @@
-"""
-Simple script to override default config values from command line.
 
-Usage:
-$ python train.py --batch_size=32 --learning_rate=1e-4
-"""
+# Config for reproduction
+graph_d = 100
+graph_l = 5
+graph_vocab_size = 'auto' # corresponds to 1max
+graph_holdout_percentage = 0.2
+num_pause_tokens = 5
+use_undirected = True
+use_directional_tokens = True # from values [false, true], picking true
+use_task_tokens = True # picking true
+weight_tying = True # Not in get_default_config?? Wait, need to check model.py
+interleave_dataset = True
 
-import sys
+# Model
+n_layer = 1
+n_head = 8
+n_embd = 32
+dropout = 0
+embd_dropout = 0
+bias = True
 
-# Parse command line arguments
-for arg in sys.argv[1:]:
-    if '=' not in arg:
-        # Assume it's a boolean flag
-        assert arg.startswith('--')
-        key = arg[2:]
-        if key in globals():
-            globals()[key] = True
-    else:
-        assert arg.startswith('--')
-        key, val = arg[2:].split('=')
-        
-        # Try to infer the type from the default value
-        if key in globals():
-            default_val = globals()[key]
-            if isinstance(default_val, bool):
-                globals()[key] = val.lower() in ['true', '1', 'yes']
-            elif isinstance(default_val, int):
-                globals()[key] = int(val)
-            elif isinstance(default_val, float):
-                globals()[key] = float(val)
-            else:
-                globals()[key] = val
-        else:
-            # If key doesn't exist, try to infer type from value
-            try:
-                globals()[key] = int(val)
-            except ValueError:
-                try:
-                    globals()[key] = float(val)
-                except ValueError:
-                    if val.lower() in ['true', 'false']:
-                        globals()[key] = val.lower() == 'true'
-                    else:
-                        globals()[key] = val
-
+learning_rate = 1e-3
+epochs = 500
+gradient_accumulation_steps = 1
+compile = False # Faster startup
+eval_interval = 1000
+log_interval = 1
+wandb_log = False
+device = 'cpu' # Use CPU for deterministic/easy run
